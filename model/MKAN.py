bf74398a15b9eb8c8077de446250f130d2a9a18c
flying-peanut <84313677+flying-peanut@users.noreply.github.com>
2025-11-27 21:41:37 +0800

    Delete model directory

--- model/MKAN.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f285b76022384a86ce50f579db57e7a205d680eb6b2343d661bd9ff58cec3b62
-size 9410
--- model/main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:25a16e695265f5c023a1308ef7216ebe924ba7f66b1a26efbb2f9715d03b76f8
-size 6672
